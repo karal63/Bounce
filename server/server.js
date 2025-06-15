ae43a492c40d4b61b2344220fdd320aebb2d1167
Leo <kuzmichleva1@gmail.com>
2025-06-15 11:23:14 +0200

    Store refresh token in redis database after user registers.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const router = require("./router/index");
+const redisClient = require("./redisClient");
 
 app.use(express.json());
 app.use(
@@ -12,6 +13,7 @@ app.use(
 );
 app.use("/api", router);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
+    await redisClient.connect();
     console.log(`Server is listening on port ${process.env.PORT}`);
 });
--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 const TokenService = require("../services/token-service");
+const redisClient = require("../redisClient");
 const token = new TokenService();
+const uuid = require("uuid");
 
 class UserService {
     async signup(email, password, name) {
@@ -11,6 +13,17 @@ class UserService {
         // ===
 
         const tokens = await token.generateTokens(email, name);
+
+        // ===
+        // store refresh key in redis
+        // ===
+        const sessionId = uuid.v4();
+        await redisClient.set(
+            `refresh_token:user:${name}:${sessionId}`,
+            tokens.refreshToken,
+            "EX",
+            30 * 24 * 60 * 60 * 1000
+        );
 
         return { ...tokens };
     }
