c90ff3a53a5d5f05bd92b2a990bc47745f8c4139
Leo <kuzmichleva1@gmail.com>
2025-07-01 18:39:22 +0200

    Create window for situation if no chat selected.

--- client/src/entities/Chat/ui/Sidebar/index.vue
+++ client/src/entities/Chat/ui/Sidebar/index.vue
@@ -24,11 +24,14 @@ const setGroup = async (room: string) => {
     <div class="min-w-[200px] max-w-[250px] h-full flex-col justify-between">
         <div>
             <!-- logo -->
-            <div class="flex-col gap-1">
+            <button
+                @click="currentChatStore.currentRoom = ''"
+                class="flex-col gap-1 cursor-pointer"
+            >
                 <div class="bg-purple-500 w-6 h-2 rounded-xl -ml-1"></div>
                 <div class="bg-purple-500 w-6 h-2 rounded-xl"></div>
                 <div class="bg-purple-500 w-6 h-2 rounded-xl ml-1"></div>
-            </div>
+            </button>
 
             <!-- chats -->
             <h1 class="text-3xl font-light mt-6">Chats</h1>
