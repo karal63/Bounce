b8f608f4113ecc9567cbb648cf5fc06f88337870
Leo <kuzmichleva1@gmail.com>
2025-07-11 13:07:23 +0200

    implement click outside hook in ProfileContext component

--- client/src/widgets/sidebar/ui/ProfileContext.vue
+++ client/src/widgets/sidebar/ui/ProfileContext.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { sidebarStore } from "../model/sidebarStore";
+import { useClickOutside } from "@/shared/lib/hooks/useClickOutside";
 const sidebar = sidebarStore();
+
+const contextRef = ref<HTMLElement | null>(null);
 
 const emit = defineEmits<{
     (event: "logout"): void;
 }>();
+
+useClickOutside(contextRef, () => {
+    sidebar.isProfileContextOpen = false;
+});
 </script>
 
 <template>
     <div
+        ref="contextRef"
         v-if="sidebar.isProfileContextOpen"
         class="absolute bottom-[120%] left-0 w-full rounded-md border border-mainBorder bg-mainDarkBg"
     >
