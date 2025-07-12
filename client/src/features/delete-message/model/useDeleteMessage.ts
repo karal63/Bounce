7916a3a83853c03837577d3143e2d5e269d63b66
Leo <kuzmichleva1@gmail.com>
2025-07-12 18:09:26 +0200

    delete deleted messages from the screen

--- client/src/features/delete-message/model/useDeleteMessage.ts
+++ client/src/features/delete-message/model/useDeleteMessage.ts
@@ -1,9 +1,15 @@
 import { apiDeleteMessage } from "@/shared/api/message/deleteMessage";
+import { useCurrentChatStore } from "@/shared/model/currentChatStore";
 
 export const useDeleteMessage = () => {
+    const currentChatStore = useCurrentChatStore();
+
     const deleteMessage = async (messageId: number) => {
         try {
             await apiDeleteMessage(messageId);
+            currentChatStore.messages = currentChatStore.messages.filter(
+                (msg) => msg.id !== messageId
+            );
         } catch (error) {
             console.log(error);
         }
