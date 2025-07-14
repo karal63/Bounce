ee67a1dce80e92838bb23cd431b3c3053e994d77
Leo <kuzmichleva1@gmail.com>
2025-07-14 20:37:33 +0200

    make group icon purple on groups list in sidebar if selected

--- client/src/features/chat-groups/ui/SingleGroup.vue
+++ client/src/features/chat-groups/ui/SingleGroup.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
+import { useCurrentChatStore } from "@/shared/model/currentChatStore";
 import type { Group } from "@/shared/types/Group";
 import { Icon } from "@iconify/vue";
+import { watchEffect } from "vue";
+
+const currentChatStore = useCurrentChatStore();
 
 const props = defineProps<{
     group: Group;
@@ -17,8 +21,15 @@ const emit = defineEmits<{
         @contextmenu="emit('setContextGroup', props.group)"
         class="py-3 cursor-pointer flex items-center gap-2 hover:text-purple-500 transition-all"
     >
-        <Icon icon="lets-icons:chat-light" class="text-3xl text-grayDull" />{{
-            group.name
-        }}
+        <Icon
+            icon="lets-icons:chat-light"
+            class="text-3xl"
+            :class="
+                currentChatStore.currentRoom === group.id
+                    ? 'text-purple-500'
+                    : 'text-grayDull'
+            "
+        />
+        {{ group.name }}
     </li>
 </template>
