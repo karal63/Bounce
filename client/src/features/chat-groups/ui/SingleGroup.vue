bac6a59ea5a0cf85bcbf34a5d010d7f655365998
Leo <kuzmichleva1@gmail.com>
2025-07-20 12:45:17 +0200

    make groups in groups list a bit smaller

--- client/src/features/chat-groups/ui/SingleGroup.vue
+++ client/src/features/chat-groups/ui/SingleGroup.vue
@@ -2,7 +2,6 @@
 import { useCurrentChatStore } from "@/shared/model/currentChatStore";
 import type { Group } from "@/shared/types/Group";
 import { Icon } from "@iconify/vue";
-import { watchEffect } from "vue";
 
 const currentChatStore = useCurrentChatStore();
 
@@ -19,7 +18,7 @@ const emit = defineEmits<{
     <li
         @click="emit('setGroup')"
         @contextmenu="emit('setContextGroup', props.group)"
-        class="py-3 cursor-pointer flex items-center gap-2 hover:text-purple-500 transition-all"
+        class="py-2 cursor-pointer flex items-center gap-2 hover:text-purple-500 transition-all"
     >
         <Icon
             icon="lets-icons:chat-light"
