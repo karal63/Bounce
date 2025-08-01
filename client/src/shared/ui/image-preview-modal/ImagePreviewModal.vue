b578b9c10c2c400c090e4d9700011583c9a9f061
Leo <kuzmichleva1@gmail.com>
2025-08-01 19:06:29 +0200

    create download button on image preview modal to download attachment

--- client/src/shared/ui/image-preview-modal/ImagePreviewModal.vue
+++ client/src/shared/ui/image-preview-modal/ImagePreviewModal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useImagePreviewStore } from "@/features/image-preview";
 import ModalTransition from "../ModalTransition.vue";
+import { Icon } from "@iconify/vue";
 
 const imagePreviewStore = useImagePreviewStore();
 </script>
@@ -14,6 +15,22 @@ const imagePreviewStore = useImagePreviewStore();
         >
             <div class="max-w-[300px]">
                 <img :src="imagePreviewStore.previewImage.image?.imageUrl" />
+            </div>
+
+            <div class="absolute top-6 flex items-center gap-2">
+                <a
+                    :download="imagePreviewStore.previewImage.image?.imageUrl"
+                    href=""
+                    title="ImageName"
+                    class="text-3xl cursor-pointer bg-mainGray px-3 rounded-md py-1 hover:bg-white/20 transition-all"
+                >
+                    <Icon icon="circum:save-down-2" />
+                </a>
+                <button
+                    class="text-3xl cursor-pointer bg-mainGray px-3 rounded-md py-1 hover:bg-white/20 transition-all"
+                >
+                    <Icon icon="mdi-light:share" />
+                </button>
             </div>
         </div>
     </ModalTransition>
