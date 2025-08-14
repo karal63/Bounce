c88f48b75e359f21c44830b7d8045cead2c45407
leo <kuzmichleva1@gmail.com>
2025-08-14 13:19:52 +0200

    prepare everything for e2e tests and make simple test checking title

--- client/src/features/send-messege/__tests__/sendMessage.e2e.test.ts
+++ client/src/features/send-messege/__tests__/sendMessage.e2e.test.ts
@@ -1,5 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 test("user can send a message", async ({ page }) => {
-    await page.goto("https://example.com");
+    await page.goto("/login");
+    await expect(page).toHaveTitle(/Bounce/);
+
+    // await page.getByTestId("login-button").click();
+    // await expect(page).toHaveURL("http://localhost:5173/login");
 });
