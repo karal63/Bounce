4676b07cc281f4fa882553aeae5d549168e815ac
leo <kuzmichleva1@gmail.com>
2025-08-20 14:27:44 +0200

    add pad to message sent time

--- client/src/shared/lib/helpers/getTime.ts
+++ client/src/shared/lib/helpers/getTime.ts
@@ -1,4 +1,7 @@
 export const getTime = (timeString: Date) => {
     const sentAt = new Date(timeString);
-    return `${sentAt.getHours()}:${sentAt.getMinutes()}`;
+    return `${sentAt.getHours().toString().padStart(2, "0")}:${sentAt
+        .getMinutes()
+        .toString()
+        .padEnd(2, "0")}`;
 };
