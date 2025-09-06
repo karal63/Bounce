52979c88fcf8a984167f100999c03c7be733e077
leo <kuzmichleva1@gmail.com>
2025-09-06 15:09:43 +0200

    create fresh ui for incoming call window

--- client/src/features/incoming-call-window/ui/IncomingCallWindow/IncomingCallWindow.vue
+++ client/src/features/incoming-call-window/ui/IncomingCallWindow/IncomingCallWindow.vue
@@ -6,6 +6,8 @@ import { onMounted, onUnmounted } from "vue";
 import { useSocket } from "@/shared/config/useSocketStore";
 import { findMessagedUserById } from "@/shared/lib/helpers";
 import { useCallStore } from "@/features/call-window/@";
+import ModalTransition from "@/shared/ui/ModalTransition.vue";
+import UserAvatar from "@/shared/ui/UserAvatar.vue";
 
 const { socket } = useSocket();
 const incomingCallStore = useInclomingCallStore();
@@ -52,37 +54,61 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <DefaultModal
-        v-show="incomingCallStore.incomingCall.isCalling"
-        v-if="incomingCallStore.incomingCall.callingUserId"
-        :event="
-            findMessagedUserById(incomingCallStore.incomingCall.callingUserId)
-                ?.otherUserName + ' calling'
-        "
-        size-x="400"
-        size-y="150"
-        :store-state="incomingCallStore.incomingCall.isCalling"
-    >
-        <div class="flex items-center justify-center gap-4">
-            <button
-                @click="incomingCallStore.accept"
-                class="bg-green-500 w-13 h-13 rounded-full text-3xl flex-center cursor-pointer transition-all"
+    <ModalTransition>
+        <div
+            v-show="incomingCallStore.incomingCall.isCalling"
+            v-if="incomingCallStore.incomingCall.callingUserId"
+            class="absolute w-full h-full left-0 top-0 flex-center bg-black/40"
+        >
+            <div
+                class="relative w-[300px] h-[325px] bg-mainGray p-2 flex-col justify-between"
             >
-                <Icon icon="proicons:call" />
-            </button>
+                <div class="mt-5 flex-col w-full items-center">
+                    <UserAvatar
+                        alt="user"
+                        size="100"
+                        :src="
+                            findMessagedUserById(
+                                incomingCallStore.incomingCall.callingUserId
+                            )?.avatarUrl
+                        "
+                    />
+                    <h3 class="text-lg mt-2">
+                        {{
+                            findMessagedUserById(
+                                incomingCallStore.incomingCall.callingUserId
+                            )?.otherUserName ?? "Unknown user"
+                        }}
+                    </h3>
+                    <p class="mt-1 text-secondary">
+                        Incoming
+                        {{ incomingCallStore.incomingCall.type }} call...
+                    </p>
+                </div>
+                <div class="flex-col gap-2">
+                    <button
+                        @click="incomingCallStore.accept"
+                        class="bg-green-500 hover:bg-green-600 w-full h-10 rounded-md flex-center cursor-pointer gap-2 transition-all"
+                    >
+                        <Icon icon="proicons:call" class="text-3xl" />
+                        <span class="text-lg">Join</span>
+                    </button>
 
-            <button
-                @click="incomingCallStore.decline"
-                class="bg-red-500 w-13 h-13 rounded-full text-4xl flex-center cursor-pointer"
-            >
-                <Icon icon="fluent:call-end-16-regular" />
-            </button>
+                    <button
+                        @click="incomingCallStore.decline"
+                        class="bg-gray-600 hover:bg-gray-700 w-full h-10 rounded-md flex-center cursor-pointer gap-2 text-lg transition-all"
+                    >
+                        Decline
+                    </button>
+                </div>
 
-            <button
-                class="absolute right-4 top-4 rounded-full text-3xl flex-center cursor-pointer"
-            >
-                <Icon icon="ic:round-close" />
-            </button>
+                <button
+                    @click="incomingCallStore.decline"
+                    class="absolute right-4 top-4 rounded-full text-3xl flex-center cursor-pointer"
+                >
+                    <Icon icon="ic:round-close" />
+                </button>
+            </div>
         </div>
-    </DefaultModal>
+    </ModalTransition>
 </template>
