37cdb12d71e2eb02d287403cd2d824e2c06534d6
leo <kuzmichleva1@gmail.com>
2026-03-15 21:44:48 +0100

    add env variables to ci

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const router = require("./router/index");
 const errorMiddleware = require("./middlewares/error-middleware");
 const redisClient = require("./redisClient");
 
-app.get("/", (req, res, next) => res.send("OK"));
-
 // Routes
 app.use("/api", router);
 app.use(errorMiddleware);
 
+app.get("/", (req, res, next) => res.send("OK"));
+
 // Server start
-server.listen(5000, async () => {
+server.listen(process.env.PORT ?? 3000, async () => {
     await redisClient.connect();
-    console.log(`Server is listening on port 5000`);
+    console.log(`Server is listening on port ${process.env.PORT}}`);
 });
